d0b4bc8fb48c120d56e0ea44f59f9ad6230089bc
kelseylo <kelseylo326@gmail.com>
2022-08-16 13:55:10 -0400

    added a label

--- Camera&DisplayPhoto/ViewController.swift
+++ Camera&DisplayPhoto/ViewController.swift
@@ -7,13 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var imagePicker = UIImagePickerController()
+    @IBOutlet weak var newlabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func choosePhotoTapped(_ sender: Any) {
+        
+        imagePicker.sourceType = .photoLibrary //opens photo library
+        
+        present(imagePicker, animated: true, completion: nil )
+    }
+    
+    @IBAction func takeSelfTapped(_ sender: Any) {
+        imagePicker.sourceType = .camera //opens camera
+        present(imagePicker, animated:true, completion: nil)
+        
+    }
+    
+    @IBOutlet weak var displayImage: UIImageView!
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        //update the photo with the selected photo
+        
+        //go back to our ViewController so the user can SEE the update
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            displayImage.image = selectedImage
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
 }
 
